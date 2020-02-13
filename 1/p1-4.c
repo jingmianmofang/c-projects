6fc25021ebf615903fa9fd34274f6f80644fc568
shaw-51 <ws2318158883@163.com>
2020-02-13 09:59:24 +0800

    day 1

---
+++ 1/p1-4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void){
+    int a;
+    a=3.4;
+    printf("%d\n",a);
+    a=4512;
+    printf("%f",a);
+    return 0;
+}
---
+++ 1/p1-8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+    int a, b;
+    int c;
+    puts("请输入两个整数。");
+    printf("整数1:"); scanf("%d", &a);
+    printf("整数2:"); scanf("%d", &b);
+    c = a * b;
+    printf("它们的乘积是%d\n", c);
+    return 0;
+}
